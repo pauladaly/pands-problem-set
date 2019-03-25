cccbfd60cea7cd6f9e63eebd9462d69f98ee1716
Daly <paula.daly@dxc.com>
2019-03-25 19:24:44 +0000

    Update Problem Set

--- solution-1.py
+++ solution-1.py
@@ -1,2 +1,15 @@
 # Paula Daly Solution to Problem 1
-# Update to test pushing to Git
+i = int(input("Please enter a positive integer: "))
+total = 0
+try:
+  while i > 0:
+    total = total + i
+    i = i - 1
+
+  if i < 0:
+    print("Please retry with a positive integer")
+
+except ValueError:
+    print("Please retry with a positive integer")  
+
+print(total)
---
+++ solution-2.py
@@ -0,0 +1,10 @@
+# Paula Daly Solution to Problem 2
+
+import datetime
+
+if datetime.datetime.today().weekday() == 1:
+  print("Yes - today begins with a T.")
+elif datetime.datetime.today().weekday() == 3:
+  print("Yes - today begins with a T.")
+else:
+  print("No - today does not begin with a T.")

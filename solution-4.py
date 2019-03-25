e097da638bc2264f7aba969e83a617c8589db948
Daly <paula.daly@dxc.com>
2019-03-25 22:33:59 +0000

    solution4

---
+++ solution-4.py
@@ -0,0 +1,19 @@
+# Paula Daly Solution to Problem 5
+# Collatz
+
+def collatz(number):
+    if number % 2 == 0:
+        print(number // 2)
+        return number // 2
+
+    elif number % 2 != 0:
+        result = 3 * number + 1
+        print(result)
+        return result
+
+try:
+    n = input("Enter number: ")
+    while n != 1:
+        n = collatz(int(n))
+except ValueError:
+    print('Please enter a positive integer')

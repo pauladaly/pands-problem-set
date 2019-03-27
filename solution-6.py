60ed5676d63a2d6ceb4424f57a05f89d73181e18
Daly <paula.daly@dxc.com>
2019-03-27 10:30:15 +0000

    update 27th Mar

---
+++ solution-6.py
@@ -0,0 +1,7 @@
+# Paula Daly Solution to Problem 5
+# strings
+sentence = input("Please enter a sentence: ")
+# spliting the sentance entered
+words = sentence.split(' ')
+# outputing ever second word
+print(words[::2])
---
+++ solution-7.py
@@ -0,0 +1,10 @@
+# Paula Daly Solution to Problem 5
+# Square root
+# import math module 
+import math
+num = float(input("Please enter a positive number: "))
+if num > 0:
+    print (f"the square root of {num} is approx.")
+    print (math.sqrt(num))
+else:
+    print(num, "is not a prime number")

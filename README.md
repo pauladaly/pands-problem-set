15a844446cabd24b13fc82b37f63c1aa5dab8a40
Daly <paula.daly@dxc.com>
2019-03-31 14:47:15 +0100

    read me

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# pands-problem-set
+# Problem Set Solutions
+
+This repository contains my solutions to the Problem Set 209 for the module Program and Scripting at GMIT.
+
+[See here for Instructions] https://github.com/ianmcloughlin/problems-pands-2019/raw/master/problems.pdf
+
+## How to download this repository:
+
+1. Go to GitHub
+2. Click the download button
+
+## How to run the code
+
+## What each file contains
+
+## References
--- solution-9.py
+++ solution-9.py
@@ -1,5 +1,11 @@
 # Paula Daly Solution to Problem 9
 # Reading Text files
-with open("moby-dick.txt", "r") as f:
-  for l in f.read().split("\n")[1::2]: # for loop to iterate through each line and slicing ot get every 2nd line
-    print(l)
+# Command line argument
+
+import sys
+if len(sys.argv) == 2:
+  with open(sys.argv[1], "r") as f:
+    for l in f.read().split("\n")[1::2]: # for loop to iterate through each line and slicing ot get every 2nd line
+      print(l)
+else:
+  print("Please include txt file name moby-dick.txt in command line")
